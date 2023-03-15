d4f0c93f18d5f4838efebe814dcb241ae0f660b1
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-15 10:19:55 +0530

    Add files via upload

---
+++ incrementoperator/src/incrementoperator/test.java
@@ -0,0 +1,17 @@
+package incrementoperator;
+
+public class test {
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=20,c=30,d=40;
+		{
+			System.out.println("preincrement="+ ++a);
+			System.out.println("predecrement="+ --b);
+			System.out.println("postincrement="+ c++);
+			System.out.println("postdecrement="+ d--);
+		}
+		
+	}
+
+}
